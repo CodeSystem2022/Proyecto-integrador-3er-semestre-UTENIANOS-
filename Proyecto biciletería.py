b4ca72000e73c302e9e4a867b99fab92a767cca2
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2023-06-29 17:44:48 -0300

    Update Proyecto biciletería.py
    Se agrega parte de c{odigo proyecto bicicleteria en .py

--- Proyecto biciletería.py
+++ Proyecto biciletería.py
@@ -1,1 +1,26 @@
+from pymongo import MongoClient
+
+class Bicicleta:
+    def _init_(self, nro_de_serie, modelo, anio, precio):
+        self.nro_de_serie = nro_de_serie
+        self.modelo = modelo
+        self.anio = anio
+        self.precio = precio
+
+    def _str_(self):
+        return f"n° de serie: {self.nro_de_serie}, modelo: {self.modelo}, año: {self.anio}, precio: ${self.precio}"
+
+# Creamos la base de datos y la colección
+class Bicicleteria:
+    def _init_(self):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client['bicicleteria']
+  # Eliminar la colección 'bicicletas' si existe
+        if 'bicicletas' in self.db.list_collection_names():
+            self.db.drop_collection('bicicletas')
+
+        self.collection = self.db['bicicletas'] # Creamos la colección
+        self.ganancias = 0  # Agregar el atributo ganancias
+        self.cantidad_de_ventas = 0  # Agregar el atributo cantidad_de_ventas
+        print("Bicicleteria creada")
 
